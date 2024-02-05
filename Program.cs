c357e6cd84ca4f4b22b30a2a03bcef74c9e3e553
包子 <chengbaozi6@qq.com>
2024-02-06 00:38:57 +0800

    WebClient

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿public static class WebClient
+{
+    public static void Main()
+    {
+
+
+        //客服端
+        //每次链接，只能进行一次请求
+        HttpClient clien = new HttpClient();
+
+        clien.BaseAddress = new Uri("http://localhost:5218/");
+
+        try
+        {
+            HttpResponseMessage respone = clien.GetAsync("/").Result;
+            byte[] data = respone.Content.ReadAsByteArrayAsync().Result;
+            Console.WriteLine(data.Length);
+            string resString = respone.Content.ReadAsStringAsync().Result;
+            Console.WriteLine(resString);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+    }
+}
